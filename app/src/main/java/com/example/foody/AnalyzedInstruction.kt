122bbc947f2384740ec169ac0573082124d0ce5a
Sanjay Singh Rathor <aitsanjay6129@gmail.com>
2022-11-20 16:27:40 +0530

    First Commit

---
+++ app/src/main/java/com/example/foody/AnalyzedInstruction.kt
@@ -0,0 +1,11 @@
+package com.example.foody
+
+
+import com.google.gson.annotations.SerializedName
+
+data class AnalyzedInstruction(
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("steps")
+    val steps: List<Step>
+)
---
+++ app/src/main/java/com/example/foody/MissedIngredient.kt
@@ -0,0 +1,29 @@
+package com.example.foody
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MissedIngredient(
+    @SerializedName("aisle")
+    val aisle: String,
+    @SerializedName("amount")
+    val amount: Double,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("meta")
+    val meta: List<String>,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("original")
+    val original: String,
+    @SerializedName("originalName")
+    val originalName: String,
+    @SerializedName("unit")
+    val unit: String,
+    @SerializedName("unitLong")
+    val unitLong: String,
+    @SerializedName("unitShort")
+    val unitShort: String
+)
